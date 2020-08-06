347388d6aa121344c8dad189bdbd435ab0b771b5
Rohit <rohitkumar377024@gmail.com>
2020-08-06 19:24:10 +0530

    Created Basic Full-width Card

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './App.css';
-import logo from './assets/images/logo.svg';
-import splash from './assets/images/beach-work.jpg';
 
 function App() {
   return (
-    <div className="px-8 py-12">
-      <img className="h-10" src={logo} alt="Workcation" />
-      <img className="mt-6 rounded-lg shadow-xl" src={splash} alt="Splash" />
-      <h1 className="mt-6 text-2xl font-bold text-gray-900 leading-tight">
-        You can work from anywhere.
-        <span className="text-indigo-500">Take advantage of it.</span>
-      </h1>
-      <p className="mt-2 text-gray-600">
-        Workcation helps you find work-friendly rentals in beautiful locations so you
-        can enjoy some nice weather even when you are not on a vacation.
+    <div className="bg-gray-900 text-white p-8">
+      <p className="text-gray-600 text-sm">Article • Aug 05, 2020</p>
+      <h1 className="text-2xl font-bold">Tailwind CSS</h1>
+      <p className="font-light">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem
+        ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
       </p>
-      <div className="mt-4">
-        <a
-          href="#"
-          className="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-sm text-white uppercase tracking-wider font-semibold"
-        >
-          Book your escape
-        </a>
+      <div className="flex mt-4">
+        <p className="border-2 border-white rounded-full text-sm px-4 py-2 mr-2">
+          Tailwind
+        </p>
+        <p className="border-2 border-white rounded-full text-sm px-4 py-2 mr-2">
+          React
+        </p>
+        <p className="border-2 border-white rounded-full text-sm px-4 py-2">Redux</p>
       </div>
     </div>
   );
