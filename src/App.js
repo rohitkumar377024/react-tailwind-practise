41eb7c917864175970301228b05484c86df49252
Rohit <rohitkumar377024@gmail.com>
2020-08-06 10:47:02 +0530

    Added: Splash Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import './App.css';
+import logo from './assets/images/logo.svg';
+import splash from './assets/images/beach-work.jpg';
 
 function App() {
   return (
-    <>
-      <h2 className="text-yellow-500">Tailwind Practise</h2>
-    </>
+    <div className="px-8 py-12">
+      <img className="h-10" src={logo} alt="Workcation" />
+      <img className="mt-6 rounded-lg shadow-xl" src={splash} alt="Splash" />
+      <h1 className="mt-6 text-2xl font-bold text-gray-900 leading-tight">
+        You can work from anywhere.
+        <span className="text-indigo-500">Take advantage of it.</span>
+      </h1>
+      <p className="mt-2 text-gray-600">
+        Workcation helps you find work-friendly rentals in beautiful locations so you
+        can enjoy some nice weather even when you are not on a vacation.
+      </p>
+      <div className="mt-4">
+        <a
+          href="#"
+          className="inline-block px-5 py-3 rounded-lg shadow-lg bg-indigo-500 text-sm text-white uppercase tracking-wider font-semibold"
+        >
+          Book your escape
+        </a>
+      </div>
+    </div>
   );
 }
 
